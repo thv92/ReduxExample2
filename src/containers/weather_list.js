5000be3408e78a2071a955d52c20410c9f9ac642
Thien Vo <vohuythien@utexas.edu>
2016-06-18 22:16:40 -0400

    created weather list container and connected it to weather state

---
+++ src/containers/weather_list.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+
+  render () {
+
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+             <th>City</th>
+             <th>Temperature</th>
+             <th>Pressure</th>
+             <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+
+      </table>
+
+    );
+
+
+  }
+
+
+}
+
+// //weather from weather reducer
+// function mapStateToProps(state) {
+//   return {weather: state.weather};
+// }
+
+//ES6
+function mapStateToProps({ weather }) {
+  return { weather }; // { weather } === { weather: weather }
+}
+
+export default connect(mapStateToProps)(WeatherList);
